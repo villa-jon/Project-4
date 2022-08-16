bc5695359df05418bb702c8bafacfffcc0cdc0eb
crystal <noelani.villanueva@gmail.com>
2022-08-16 13:07:49 -0700

    added react portion to topics

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do 
   resources :users
   resources :cards 
+  resources :topics
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
