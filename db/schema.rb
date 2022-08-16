b17e1d7d2f361a4e8e9ba2af1353ae0a755aec24
crystal <noelani.villanueva@gmail.com>
2022-08-16 13:06:23 -0700

    added topics

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_192026) do
+ActiveRecord::Schema.define(version: 2022_08_16_200555) do
 
   create_table "cards", force: :cascade do |t|
     t.string "topic"
     t.text "body"
     t.string "answer"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
